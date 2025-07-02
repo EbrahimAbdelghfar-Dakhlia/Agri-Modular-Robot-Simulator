6e7318bc40dc4eaf255ca7c0e98aa283dc5b3ab4
ebrahimabdelghfar <ebrahimabdelghfar550@gmail.com>
2025-07-02 10:04:27 +0300

    [adjust the steering controller in opposite phase module]

--- src/modular_robot_description/modular_robot_description/joy_controller.py
+++ src/modular_robot_description/modular_robot_description/joy_controller.py
@@ -19,12 +19,14 @@ class Commander(Node):
         global extending_length
         super().__init__('commander')
         timer_period = 0.02
-        self.wheel_seperation = 1.082 
-        self.wheel_base = 0.900
-        self.wheel_radius = 0.1
+        self.WHEEL_SEPERATION = 1.082 
+        self.WHEEL_BASE = 0.900
+        self.WHEEL_RADIUS = 0.1
         self.wheel_steering_y_offset = 0.0
-        self.steering_track = self.wheel_seperation - 2 * self.wheel_steering_y_offset
+        self.steering_track = self.WHEEL_SEPERATION - 2 * self.wheel_steering_y_offset
         self.new_steering_track = self.steering_track
+        self.turn_radius = 0.0 
+        self.MAX_STEERING_ANGLE = math.pi/4
 
         self.pos = np.array([0,0,0,0], float) #[rear_right_steering, rear_left_steering, front_right_steering, front_left_steering] in radians
         self.vel = np.array([0,0,0,0], float) #[rear_right_velocity, rear_left_velocity, front_right_velocity, front_left_velocity] in radians/sec
@@ -43,15 +45,24 @@ class Commander(Node):
             vel_steerring_offset = vel_msg.angular.z * self.wheel_steering_y_offset
             sign = np.sign(vel_msg.linear.x)
 
-            self.vel[0] = sign*math.hypot(vel_msg.linear.x - vel_msg.angular.z*self.new_steering_track/2, vel_msg.angular.z*self.wheel_base/2) - vel_steerring_offset
-            self.vel[1] = sign*math.hypot(vel_msg.linear.x + vel_msg.angular.z*self.new_steering_track/2, vel_msg.angular.z*self.wheel_base/2) + vel_steerring_offset
-            self.vel[2] = sign*math.hypot(vel_msg.linear.x - vel_msg.angular.z*self.new_steering_track/2, vel_msg.angular.z*self.wheel_base/2) - vel_steerring_offset
-            self.vel[3] = sign*math.hypot(vel_msg.linear.x + vel_msg.angular.z*self.new_steering_track/2, vel_msg.angular.z*self.wheel_base/2) + vel_steerring_offset
+            if abs(vel_msg.angular.z) > 0.01:
+                self.turn_radius = self.WHEEL_BASE / (2.0*abs(vel_msg.angular.z))
+
+            self.vel[0] = sign*math.hypot(vel_msg.linear.x - vel_msg.angular.z*self.new_steering_track/2, vel_msg.angular.z*self.WHEEL_BASE/2) - vel_steerring_offset
+            self.vel[1] = sign*math.hypot(vel_msg.linear.x + vel_msg.angular.z*self.new_steering_track/2, vel_msg.angular.z*self.WHEEL_BASE/2) + vel_steerring_offset
+            self.vel[2] = sign*math.hypot(vel_msg.linear.x - vel_msg.angular.z*self.new_steering_track/2, vel_msg.angular.z*self.WHEEL_BASE/2) - vel_steerring_offset
+            self.vel[3] = sign*math.hypot(vel_msg.linear.x + vel_msg.angular.z*self.new_steering_track/2, vel_msg.angular.z*self.WHEEL_BASE/2) + vel_steerring_offset
             if(vel_msg.angular.z != 0):
-                self.pos[0] = math.atan2(vel_msg.angular.z*self.wheel_base,(2*vel_msg.linear.x - vel_msg.angular.z*self.new_steering_track))
-                self.pos[1] = math.atan2(vel_msg.angular.z*self.wheel_base,(2*vel_msg.linear.x - vel_msg.angular.z*self.new_steering_track))
-                self.pos[2] =  -self.pos[0]
-                self.pos[3] =  -self.pos[1]
+                if (vel_msg.angular.z<0):
+                    self.pos[0] = np.clip(math.atan2(self.WHEEL_BASE,(self.turn_radius+self.new_steering_track/2)),-self.MAX_STEERING_ANGLE,self.MAX_STEERING_ANGLE)
+                    self.pos[1] = np.clip(math.atan2(self.WHEEL_BASE,(self.turn_radius-self.new_steering_track/2)),-self.MAX_STEERING_ANGLE,self.MAX_STEERING_ANGLE)
+                    self.pos[2] =  -self.pos[0] 
+                    self.pos[3] =  -self.pos[1]
+                else:
+                    self.pos[0] = -np.clip(math.atan2(self.WHEEL_BASE,(self.turn_radius+self.new_steering_track/2)),-self.MAX_STEERING_ANGLE,self.MAX_STEERING_ANGLE)
+                    self.pos[1] = -np.clip(math.atan2(self.WHEEL_BASE,(self.turn_radius-self.new_steering_track/2)),-self.MAX_STEERING_ANGLE,self.MAX_STEERING_ANGLE)
+                    self.pos[2] = -self.pos[0]
+                    self.pos[3] = -self.pos[1]       
             else:
                 self.pos[0] = 0
                 self.pos[1] = 0
@@ -79,10 +90,10 @@ class Commander(Node):
         # pivot turn
         elif(mode_selection == 3):
 
-            self.pos[0] = math.atan(self.wheel_base/self.new_steering_track)
-            self.pos[1] = -math.atan(self.wheel_base/self.new_steering_track)
-            self.pos[2] = -math.atan(self.wheel_base/self.new_steering_track)
-            self.pos[3] = math.atan(self.wheel_base/self.new_steering_track)
+            self.pos[0] = math.atan2(self.WHEEL_BASE,self.new_steering_track)
+            self.pos[1] = -math.atan2(self.WHEEL_BASE,self.new_steering_track)
+            self.pos[2] = -math.atan2(self.WHEEL_BASE,self.new_steering_track)
+            self.pos[3] = math.atan2(self.WHEEL_BASE,self.new_steering_track)
             
             self.vel[0] = -vel_msg.angular.z
             self.vel[1] = vel_msg.angular.z
@@ -98,7 +109,6 @@ class Commander(Node):
         vel_array = Float64MultiArray(data=self.vel) 
         self.pub_pos.publish(pos_array)
         self.pub_vel.publish(vel_array)
-        print(f'Publishing pos: {self.pos}, vel: {self.vel}, extending_length: {extending_length}, elevating_length: {elevating_length}',mode_selection )
         self.extending.publish(Float64(data=extending_length))
         self.elevating.publish(Float64(data=elevating_length))
         self.pos[:] = 0
@@ -128,19 +138,19 @@ class Joy_subscriber(Node):
             mode_selection = mode_selection
 
         # Increment the extending and elevating lengths periodically
-        if (self.joy_controller.buttons[4]==1 and extending_length <= 0.5):
+        if (self.joy_controller.buttons[4] == 1 and extending_length <= 0.5):
             extending_length +=0.01
-        elif (self.joy_controller.buttons[6]==1 and extending_length >= 0.0):
+        elif (self.joy_controller.buttons[6] == 1 and extending_length >= 0.0):
             extending_length -= 0.01
         
-        if (self.joy_controller.buttons[5]==1 and elevating_length <= 0.4):
+        if (self.joy_controller.buttons[5] == 1 and elevating_length <= 0.4):
             elevating_length +=0.01
         elif (self.joy_controller.buttons[7]==1 and elevating_length > 0.0):
             elevating_length -=0.01
         pass
 
-        vel_msg.linear.x  = self.joy_controller.axes[1]*7.5
-        vel_msg.linear.y  = self.joy_controller.axes[0]*7.5
+        vel_msg.linear.x  = self.joy_controller.axes[1]*1.5
+        vel_msg.linear.y  = self.joy_controller.axes[0]*1.5
         vel_msg.angular.z = self.joy_controller.axes[3]*3
 
         
